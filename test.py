c637a3feac69c50e59931abbade66b4f494e0ff4
Kirill Kozlov <kozlov.kirill.v.work@gmail.com>
2023-07-04 17:34:24 +0700

    Add CRUD for labels

--- test.py
+++ test.py
@@ -1,7 +1,25 @@
 from core.CRUD import card as CardCRUD
+from core.CRUD import label as LabelCRUD
 
 if __name__ == '__main__':
-    CardCRUD.create(1, 'Hello, worm!', 'Привет, мир!')
-    print(CardCRUD.readOne(1))
-    CardCRUD.update(1, 18, 'Fixed', 'Починено')
-    print(CardCRUD.readOne(1))
+	# try:
+	# 	CardCRUD.delete(17, 17)
+	# except PermissionError:
+	# 	print('Permission denied')
+	# LabelCRUD.create(1, 'ENG/RU', 'ENG', 'RU')
+	# LabelCRUD.create(3, 'Tech', 'Definition', 'Value', True)
+	# print(LabelCRUD.readOne(1))
+	# print(LabelCRUD.readAllUserCards(1))
+	# print(LabelCRUD.readAllUserCards(2))
+	# print(LabelCRUD.readAllUserCards(3))
+	# for i in range(8):
+	# 	try:
+	# 		LabelCRUD.delete(1, i+1)
+	# 	except PermissionError:
+	# 		print('Permission denied')
+	# print(LabelCRUD.readAllUserCards(1))
+	# print(LabelCRUD.readAllUserCards(2))
+	# print(LabelCRUD.readAllUserCards(3))
+	print(LabelCRUD.readOne(10))
+	LabelCRUD.update(3, 10, 'fixed', 'fixed1', 'fixed2')
+	print(LabelCRUD.readOne(10))
